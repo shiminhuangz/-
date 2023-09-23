6a62c6626b08620c65b1b653f8328594b204eb32
karsa <karsa@LAPTOP-EOI7AGB0>
2023-09-23 11:21:51 +0800

    加入專案檔案。

---
+++ 圓周率計算/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double pi = Math.PI;
+
+        Console.Write("請輸入圓的半徑：");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        double area = pi * Math.Pow(radius, 2);   
+        double roundedArea = Math.Round(area, 5);
+
+        Console.WriteLine($"圓的面積是：{roundedArea}");
+
+      
+        Console.ReadKey();
+    }
+}
